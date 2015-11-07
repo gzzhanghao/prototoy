4c03bdce9ecd01332c674e80f30f517c49d45716
Jason <jason.daurus@gmail.com>
2015-11-07 08:03:43 +0800

    Remove useless utils

--- src/util.js
+++ src/util.js
@@ -1,7 +1,6 @@
 var numReg = /^-?\d*\.?\d+$/;
 var defaultUnits = { top: 'px', left: 'px', right: 'px', bottom: 'px', width: 'px', height: 'px' };
 
-export var isEmpty = value => value === null || typeof value === 'undefined' || Number.isNaN(value);
 export var $ = (selector, context) => (context || document).querySelector(selector);
 export var $$ = (selector, context) => [].slice.call((context || document).querySelectorAll(selector));
 export var on = (element, event, handler) => element.addEventListener(event, handler);
@@ -9,7 +8,6 @@ export var off = (element, event, handler) => element.removeEventListener(event,
 export var has = (host, key) => host.hasOwnProperty(key);
 export var Symbol = token => `${Date.now()} ${token} ${Math.random()}`;
 export var isArray = value => value instanceof Array;
-export var isString = value => typeof value === 'string';
 export var isFunction = value => typeof value === 'function';
 export var camelize = string => string.replace(/\-\w/g, match => match.charAt(1).toUpperCase());
 export var addUnit = (name, value) => (has(defaultUnits, name) && numReg.test(value)) ? value + defaultUnits[name] : value;
