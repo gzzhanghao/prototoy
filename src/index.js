e5167564fa3e5534f24311f11e8c7176335a9b71
Jason <jason.daurus@gmail.com>
2015-11-16 13:06:34 +0800

    Update package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import assign from 'object-assign';
 import Layout from './Layout';
 import ShadowElement from './ShadowElement';
 import * as util from './util';
 
-Layout.ShadowElement = ShadowElement;
-util.assign(Layout, util);
+assign(Layout, { ShadowElement, assign }, util);
 
 export default Layout;
